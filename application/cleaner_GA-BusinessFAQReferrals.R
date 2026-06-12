6efe7913c87f7b0d0505919c9c460815a842d0c6
kevinathom <44673990+kevinathom@users.noreply.github.com>
2026-06-12 17:40:31 -0400

    Update cleaner_GA-BusinessFAQReferrals.R
    Limit main data ingest to relevant content. Rename date_new to date_string for reduced confusion. Add full table restructuring process. Expand date handling to extract year and month separately (to match allfaqviews file).

--- application/cleaner_GA-BusinessFAQReferrals.R
+++ application/cleaner_GA-BusinessFAQReferrals.R
@@ -3,25 +3,35 @@
 setwd("~/GitHub/PennLib-FAQ-PowerBI/data")
 
 dat_old <- read.csv("GA_BusinessFAQReferrals_clean.csv")
-dat_new <- read.csv("C:/Users/kevinat/Downloads/download.csv") # get the referrer and grand total
-date_new <- read.csv("C:/Users/kevinat/Downloads/download.csv",
-                     skip = 3,
-                     nrows = 1,
-                     header = FALSE)[1, 1]
+dat_new <- read.csv("C:/Users/kevinat/Downloads/download.csv",
+                    skip = 6,
+                    nrows = 3,
+                    row.names = NULL,
+                    header = FALSE)[c(1,3), -1] # get the referrer and total referrals
+date_string <- read.csv("C:/Users/kevinat/Downloads/download.csv",
+                        skip = 3,
+                        nrows = 1,
+                        header = FALSE)[1, 1]
 
 #dat_old <- read.csv(BusinessFAQReferrals_old)
 #dat_new <- read.csv(BusinessFAQReferrals_new)
 
 
-# Transpose axes
+# Restructure table
 
-t(dat_new)
+dat_new <- dat_new[ , 1:(ncol(dat_new) - 2)]
+dat_new <- t(dat_new)
+dat_new <- as.data.frame(dat_new)
+colnames(dat_new) <- c("Referrer", "Entrances")
+dat_new$Entrances <- as.integer(dat_new$Entrances)
 
 
-# Append date
+# Append year and month
 
-date_new <- substr(date_new, 3, 8) # Is this format compatible with other data?
-date_new$Date <- date_new
+dat_new$Year <- substr(date_string, 3, 6)
+month_lookup <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+month <- month_lookup[as.integer(substr(date_string, 7, 8))]
+dat_new$Month <- rep(month, nrow(dat_new))
 
 
 # Append or replace data
