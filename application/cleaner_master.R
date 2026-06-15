1c767d6737a83d3e49cce9e67b22f137bb88733a
kevinathom <44673990+kevinathom@users.noreply.github.com>
2026-06-15 16:40:09 -0400

    Update cleaner_master.R
    Fix directory structure for BusinessFAQReferrals

--- application/cleaner_master.R
+++ application/cleaner_master.R
@@ -15,7 +15,7 @@ queryanalyzer_new <- "queryanalyzer_707_2025-08-27_11_19_29.csv" # Update this f
 queryanalyzer_out <- "la_queryanalyzer_clean.csv"
 
 BusinessFAQReferrals_old <- "GA_BusinessFAQReferrals_clean.csv"
-BusinessFAQReferrals_dir <- "/GA_BusinessFAQReferrals_new"
+BusinessFAQReferrals_dir <- "./GA_BusinessFAQReferrals_new"
 BusinessFAQReferrals_out <- BusinessFAQReferrals_old
 
 
@@ -24,7 +24,9 @@ BusinessFAQReferrals_out <- BusinessFAQReferrals_old
 source("~/GitHub/PennLib-FAQ-PowerBI/application/cleaner_allfaqviews.R")
 source("~/GitHub/PennLib-FAQ-PowerBI/application/cleaner_faq_export.R")
 source("~/GitHub/PennLib-FAQ-PowerBI/application/cleaner_queryanalyzer.R")
-for(filename in list.files(BusinessFAQReferrals_dir)){
+for(filename in list.files(BusinessFAQReferrals_dir,
+                           full.names = TRUE
+                           )){
   BusinessFAQReferrals_new <- filename
   source("~/GitHub/PennLib-FAQ-PowerBI/application/cleaner_GA-BusinessFAQREferrals.R")
 }
